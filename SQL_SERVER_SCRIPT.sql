0af8646118df9fd2e88a249bb6ba93e77186b775
Cesar Santos <63975528+cesalomx@users.noreply.github.com>
2022-01-19 16:58:09 -0600

    Add files via upload

--- SQL_SERVER_SCRIPT.sql
+++ SQL_SERVER_SCRIPT.sql
@@ -3,30 +3,24 @@ GO
 	CREATE DATABASE Playstore
 GO
 	USE Playstore
-GO
-	DROP TABLE IF EXISTS info
 GO	
 	CREATE TABLE info(
 	Info_Id VARCHAR(500) PRIMARY KEY,
 	Last_Updated VARCHAR(250) NULL,
 	Content_Rating VARCHAR(250) NULL,
-	Privacy_Policy VARCHAR(1500) NULL,
+	Privacy_Policy VARCHAR(8000) NULL,
 	Ad_Supported VARCHAR(250) NULL,
 	In_App_Purchases VARCHAR(250) NULL,
 )
-GO
-	DROP TABLE IF EXISTS developer
 GO
 	CREATE TABLE developer
 	(
 	Devs_Id varchar(500) PRIMARY KEY,
 	Developer_Id varchar(500) NULL,
-	Developer_Website varchar(500) NULL,
+	Developer_Website varchar(700) NULL,
 	Developer_Email	varchar(500) NULL,
 	Editors_Choice varchar(500)	NULL
 	)
-GO
-	DROP TABLE IF EXISTS "public".installs
 GO
 	CREATE TABLE install(
 	Install_Id VARCHAR(500) PRIMARY KEY,
@@ -35,8 +29,6 @@ GO
 	Maximum_Installs VARCHAR(500) NULL,
 	Minimum_Android VARCHAR(500) NULL,
 )
-GO
-	DROP TABLE IF EXISTS app
 GO
 	CREATE TABLE app
 (
@@ -53,6 +45,7 @@ GO
 	 Info_Id      varchar(500) REFERENCES info(Info_Id) NOT NULL,
 	 Devs_Id      varchar(500) REFERENCES developer(Devs_Id) NOT NULL,
 	 Install_Id   varchar(500) REFERENCES install(Install_Id) NOT NULL,
+	 Scraped_Time timestamp
 	);
 GO
 	BULK INSERT info
@@ -82,4 +75,17 @@ GO
 		  FIELDTERMINATOR = '|',
 		  ROWTERMINATOR='\n'
 		  )
-ALTER DATABASE "Playstore" COLLATE SQL_Latin1_General_CP1_CI_AS
+
+GO
+	DROP TABLE IF EXISTS dbo.app
+	DROP TABLE IF EXISTS dbo.developer
+	DROP TABLE IF EXISTS dbo.info
+	DROP TABLE IF EXISTS dbo.install
+
+--BULK INSERT info
+--FROM 'D:\Cesar\pgAdmin - MySQL - Oracle\MSSQL\df_dim3.csv'
+--WITH (CODEPAGE = '65001', DATAFILETYPE = 'Char',FIRSTROW = 2,
+--		  FIELDTERMINATOR = '|',
+--		  ROWTERMINATOR='\n')
+
+SELECT * FROM [dbo].[info]
