4ddee18f715e037aa3f71a885293a507b9499e33
Cesar Santos <63975528+cesalomx@users.noreply.github.com>
2022-01-18 20:07:37 -0600

    Add files via upload

--- Playstore_SQL_Script.sql
+++ Playstore_SQL_Script.sql
@@ -1,3 +1,26 @@
+-- Joins (via the JOIN clause) are how we query data contained in multiple tables.
+-- There are two main types of relationships:
+--   1. One-to-many
+--   2. Many-to-many
+
+-- For example, a customer has many invoices, but each invoice belongs to one
+-- and only one customer.  This is a one-to-many relationship.
+
+-- Think of some one-to-many relationships in every day life, e.g.,
+--
+--   1. A car can get many parking tickets, but a parking ticket belongs to
+--      one and only one car.
+--   2. Every state in the US has many cities, but every city in the US is in
+--      one and only one state.
+--   3. Customers can have many orders, but an order belongs to a single customer
+--   4. Customers can have multiple credit cards, but every credit card belong
+--      to a single customer.
+--   5. Think of some of your own.
+
+-- An example of a many-to-many relationship might be events and people.
+-- I can attend many events and each event has many attendees.
+-- In our toy app database, we're only dealing with one-to-many relationships.
+
 DROP TABLE IF EXISTS app
 DROP TABLE IF EXISTS developer
 DROP TABLE IF EXISTS info
@@ -13,7 +36,7 @@ CREATE TABLE "public".info(
 	Info_Id VARCHAR(500) NOT NULL UNIQUE,
 	Last_Updated VARCHAR(500) NULL,
 	Content_Rating VARCHAR(500) NULL,
-	Privacy_Policy VARCHAR(1500) NULL,
+	Privacy_Policy VARCHAR(10000) NULL,
 	Ad_Supported VARCHAR(500) NULL,
 	In_App_Purchases VARCHAR(500) NULL,
 	CONSTRAINT PK_73 PRIMARY KEY ( Info_Id )
@@ -58,6 +81,7 @@ CREATE TABLE "public".app
 	 Info_Id      varchar(500) REFERENCES info(Info_Id) NOT NULL,
 	 Devs_Id      varchar(500) REFERENCES developer(Devs_Id) NOT NULL,
 	 Install_Id   varchar(500) REFERENCES install(Install_Id) NOT NULL,
+	 Scraped_Time timestamp,
 	 CONSTRAINT PK_7 PRIMARY KEY ( App_Id )
 	);
 
@@ -73,4 +97,23 @@ WITH DELIMITER '|' CSV HEADER;
 COPY info FROM 'D:\Cesar\pgAdmin - MySQL - Oracle\PostgreSQL\df_dim3.csv'
 WITH DELIMITER '|' CSV HEADER;
 
+SELECT * FROM app AS t1
+INNER JOIN install AS t2 ON t1.install_id = t2.install_id
+LIMIT 100
 
+SELECT * FROM install AS t1
+INNER JOIN app AS t2 ON t1.install_id = t2.install_id
+LIMIT 100
+
+SELECT * FROM "public".info
+LIMIT 100
+
+Select *
+From 
+(
+    Select 
+      Row_Number() Over () As RowNum
+    , *
+    From "public".info
+) t2
+Where RowNum = 1741259
